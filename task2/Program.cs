d1696d90492a52df7478c2062d33c07f91544bd8
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-14 18:10:37 +0400

    n2

---
+++ task2/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+Console.WriteLine("Hello, World!");
+
+#region task-1
+//int y, x, count;
+//y = 10;
+//x = 50;
+//count = 0;
+//for (int i = y; i <= x; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
+#region task-2
+//int t = 80;
+//if (t % 2 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (t % 3 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (t % 4 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (t % 5 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (t % 7 == 0)
+//{
+//    Console.WriteLine("murekkeb");
+//}
+//else if (t % 2 == 1)
+//{
+//    Console.WriteLine("sade");
+//}
+#endregion
+#region task-3
+//int a = 12;
+//int b = 3;
+//if (a%b==0)
+//{
+//    Console.WriteLine("tam bölünür");
+//}
+//else
+//{
+//    Console.WriteLine("bölünmür");
+//}
+#endregion
+#region task-4
+int j = 20;
+int k = 30;
+int l = 50;
+if (j > l && k > l)
+{
+    Console.WriteLine(j + k);
+}
+else if (j < l && k < l)
+{
+    Console.WriteLine("duzgun reqem daxil et");
+}
+#endregion
+
